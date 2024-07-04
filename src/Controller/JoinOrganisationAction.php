b3463bd17c5be16004960d576eaf3f152cd255bd
Igor <dzeroigor.dtp@gmail.com>
2024-07-04 09:58:39 +0300

    --optimize JoinOrganisationAction

--- src/Controller/JoinOrganisationAction.php
+++ src/Controller/JoinOrganisationAction.php
@@ -32,6 +32,91 @@ class JoinOrganisationAction
     ) {
     }
 
+//    public function __invoke(Request $request, string $token): Response
+//    {
+//        /** @var OrganisationMembershipInterface $member */
+//        $member = $this->organisationMembershipRepository->findOneBy(['emailVerificationToken' => $token]);
+//
+//        if (!$member) {
+//            throw new \InvalidArgumentException('invalid token');
+//        }
+//
+//        $customer = $member->getCustomer();
+//
+//        if (null !== $customer) {
+//            $this->registry->get($member, 'app_organisation_membership')->apply($member, 'accept_membership');
+//
+//            $member->getCustomer()->getUser()->isVerified(true);
+//            $member->getCustomer()->getUser()->setVerifiedAt(new \DateTime());
+//            $member->setEmailVerificationToken(null);
+//            $member->setVerified(new \DateTime());
+//
+//            $this->entityManager->persist($customer);
+//            $this->entityManager->flush();
+//
+//            $this->security->login($customer->getUser(), 'form_login', 'app');
+//
+//            return new RedirectResponse($this->router->generate('sylius_frontend_account_dashboard'));
+//        }
+//
+//        $customer = $this->customerFactory->createNew();
+//        $customer->setEmail($member->getEmail());
+//        $customer->setEmailCanonical($member->getEmail());
+//
+//        $form = $this->formFactory->create(CustomerRegistrationType::class, $customer);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $customer = $form->getData();
+//            $member->setCustomer($customer);
+//
+//            $member->getCustomer()->getUser()->isVerified(true);
+//            $member->getCustomer()->getUser()->setVerifiedAt(new \DateTime());
+//            $member->setEmailVerificationToken(null);
+//            $member->setVerified(new \DateTime());
+//
+//            $this->registry->get($member, 'app_organisation_membership')->apply($member, 'accept_membership');
+//
+//            $this->entityManager->persist($customer);
+//            $this->entityManager->flush();
+//
+//            $this->security->login($customer->getUser(), 'form_login', 'app');
+//
+//            return new RedirectResponse($this->router->generate('sylius_frontend_account_dashboard'));
+//        }
+//
+//        $content = $this->twig->render(
+//            'frontend/organisation_membership/join.html.twig',
+//            [
+//                'form' => $form->createView(),
+//            ]
+//        );
+//
+//        return new Response($content);
+//    }
+
+    private function processMember(OrganisationMembershipInterface $member): void
+    {
+        $member->getCustomer()->getUser()->isVerified(true);
+        $member->getCustomer()->getUser()->setVerifiedAt(new \DateTime());
+        $member->setEmailVerificationToken(null);
+        $member->setVerified(new \DateTime());
+    }
+
+    private function handleCustomerAndLogin(OrganisationMembershipInterface $member, $customer): RedirectResponse
+    {
+        $this->processMember($member);
+
+        $this->registry->get($member, 'app_organisation_membership')->apply($member, 'accept_membership');
+
+        $this->entityManager->persist($customer);
+        $this->entityManager->flush();
+
+        $this->security->login($customer->getUser(), 'form_login', 'app');
+
+        return new RedirectResponse($this->router->generate('sylius_frontend_account_dashboard'));
+    }
+
     public function __invoke(Request $request, string $token): Response
     {
         /** @var OrganisationMembershipInterface $member */
@@ -44,20 +129,7 @@ class JoinOrganisationAction
         $customer = $member->getCustomer();
 
         if (null !== $customer) {
-            $this->registry->get($member, 'app_organisation_membership')->apply($member, 'accept_membership');
-
-            $member->getCustomer()->getUser()->isVerified(true);
-            $member->getCustomer()->getUser()->setVerifiedAt(new \DateTime());
-            $member->setEmailVerificationToken(null);
-            $member->setVerified(new \DateTime());
-
-
-            $this->entityManager->persist($customer);
-            $this->entityManager->flush();
-
-            $this->security->login($customer->getUser(), 'form_login', 'app');
-
-            return new RedirectResponse($this->router->generate('sylius_frontend_account_dashboard'));
+            return $this->handleCustomerAndLogin($member, $customer);
         }
 
         $customer = $this->customerFactory->createNew();
@@ -70,20 +142,8 @@ class JoinOrganisationAction
         if ($form->isSubmitted() && $form->isValid()) {
             $customer = $form->getData();
             $member->setCustomer($customer);
-            $member->setEmailVerificationToken(null);
-            $member->setVerified(new \DateTime());
 
-            $this->registry->get($member, 'app_organisation_membership')->apply($member, 'accept_membership');
-
-            $member->getCustomer()->getUser()->isVerified(true);
-            $member->getCustomer()->getUser()->setVerifiedAt(new \DateTime());
-
-            $this->entityManager->persist($customer);
-            $this->entityManager->flush();
-
-            $this->security->login($customer->getUser(), 'form_login', 'app');
-
-            return new RedirectResponse($this->router->generate('sylius_frontend_account_dashboard'));
+            return $this->handleCustomerAndLogin($member, $customer);
         }
 
         $content = $this->twig->render(
@@ -95,4 +155,5 @@ class JoinOrganisationAction
 
         return new Response($content);
     }
+
 }
